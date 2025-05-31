579867b1e529c015bd56370df2bf06060016882f
Taosif Bin Murtaza <dev.taosif.net@gmail.com>
2025-05-31 23:44:13 +0600

    Updated

--- Restaurant.API/Program.cs
+++ Restaurant.API/Program.cs
@@ -5,11 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddInfrastructure(builder.Configuration);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
